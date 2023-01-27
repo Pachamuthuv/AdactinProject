7e2e871ebeabd3221358c022c5f910ed253d52cb
Herbert <103264430+Herbertpy@users.noreply.github.com>
2023-01-27 12:34:28 +0530

    Conflict

--- src/org/adactin/Login.java
+++ src/org/adactin/Login.java
@@ -1,23 +1,16 @@
 package org.adactin;
 
 public class Login {
-
-	
-	
-	
-	
-	
-	
-	
-	
 	private void methodOne() {
 		// TODO Auto-generated method stub
 
 	}
+
 	private void methodTwo() {
 		// TODO Auto-generated method stub
 
 	}
+
 	private void methodThree() {
 		// TODO Auto-generated method stub
 
