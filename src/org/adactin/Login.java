669d768b280ee0a5ebdfd50693e2e949567a9b33
Herbertpy <herbetraj784@gmail.com>
2023-01-27 12:13:17 +0530

    Three Methods Added

--- src/org/adactin/Login.java
+++ src/org/adactin/Login.java
@@ -2,4 +2,24 @@ package org.adactin;
 
 public class Login {
 
+	
+	
+	
+	
+	
+	
+	
+	
+	private void methodOne() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodTwo() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodThree() {
+		// TODO Auto-generated method stub
+
+	}
 }
