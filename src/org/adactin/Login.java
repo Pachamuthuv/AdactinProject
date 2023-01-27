e3a9149ad9e6e43240e722ac31663193175819bd
Herbert <103264430+Herbertpy@users.noreply.github.com>
2023-01-27 13:08:02 +0530

    Jocinth Added

--- src/org/adactin/Login.java
+++ src/org/adactin/Login.java
@@ -1,5 +1,14 @@
 package org.adactin;
 
 public class Login {
+	
+	private void methodJocinthOne() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodJocinthTwo() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
